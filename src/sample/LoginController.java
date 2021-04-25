f784c70ce952a77aa2e46e5fefb12e93dbdf5e32
meliwi <melissa.gonzalez@correounivalle.edu.co>
2021-04-25 16:34:44 -0500

    Initial commit

---
+++ src/sample/LoginController.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+
+public class LoginController implements Initializable {
+    @FXML
+    private Button loginbutton;
+    @FXML
+    private TextField correo;
+    @FXML
+    private TextField contraseña;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
+    @FXML
+    private void loginButtonAction(ActionEvent event) throws IOException {
+        System.out.println("You clicked me!");
+        //if (correo.getText().isBlank() == false && contraseña.getText().isBlank()==false){
+        Parent root = FXMLLoader.load(getClass().getResource("modules.fxml"));
+        Stage modulesStage = new Stage();
+        modulesStage.setTitle("Gestión de módulos");
+        modulesStage.setScene(new Scene(root, 900, 600));
+        //Para cerrar la ventana de login
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+        modulesStage.show();
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
