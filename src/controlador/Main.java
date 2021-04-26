bc0ddac06732ee5541edd2257e4108c19f12ba2b
meliwi <melissa.gonzalez@correounivalle.edu.co>
2021-04-25 20:31:04 -0500

    model view controller

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,4 +1,4 @@
-package login;
+package controlador;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/vista/login.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
