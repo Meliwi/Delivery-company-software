e59e309941c7fef9b867e13195da2ee1131cac54
kiin <nicolas.jaramillo@correounivalle.edu.co>
2021-05-06 18:09:07 -0500

    Sedes-editar complete

--- src/controlador/OfficesManagement.java
+++ src/controlador/OfficesManagement.java
@@ -44,10 +44,10 @@ public class OfficesManagement implements Initializable {
             String updaterQuery = "INSERT INTO sede(id_sede, nit_empresa, ciudad, telefono, direccion) VALUES ('" + name +"', '"+ nit + "', '" + city + "', '" + telephoneNumber + "', '" + address + "')";
 
             if (name != "" && con.GUARDAR(updaterQuery)){
-                id_sede.setText(null);
-                ciudad.setText(null);
-                telefono.setText(null);
-                direccion.setText(null);
+                id_sede.setText("");
+                ciudad.setText("");
+                telefono.setText("");
+                direccion.setText("");
                 Parent root = FXMLLoader.load(getClass().getResource("/vista/popupWindow.fxml"));
                 Stage registroStage = new Stage();
                 registroStage.setTitle("Registro");
@@ -85,32 +85,38 @@ public class OfficesManagement implements Initializable {
     private void editarButtonAction (ActionEvent event) throws IOException {
         String nit = "890399001";
         String name = id_sede.getText().trim();
-        String city = ciudad.getText().trim();
-        String telephoneNumber = telefono.getText().trim();
-        String address = direccion.getText().trim();
+        String city = ciudad.getText();
+        String telephoneNumber = telefono.getText();
+        String address = direccion.getText();
         Connect con  = new Connect();
         try{
             if(name.isEmpty()){
-                mensaje.setText("Porfavor ingrese la cédula del usuario");
-            }
-            String updaterQuery = "INSERT INTO sede(nit_empresa, ciudad, telefono, direccion) VALUES ('" + nit + "', '" + city + "', '" + telephoneNumber + "', '" + address + "')";
-            if (con.GUARDAR(updaterQuery)){
-                ciudad.setText(null);
-                telefono.setText(null);
-                direccion.setText(null);
-                Parent root = FXMLLoader.load(getClass().getResource("/vista/popupWindow.fxml"));
-                Stage registroStage = new Stage();
-                registroStage.setTitle("Registro");
-                registroStage.setScene(new Scene(root, 458, 100));
-                registroStage.show();
+                mensaje.setText("Porfavor ingrese el nombre de la sede");
             }
             else{
-                Parent root = FXMLLoader.load(getClass().getResource("/vista/alertWindow.fxml"));
-                Stage registroStage = new Stage();
-                registroStage.setTitle("Registro");
-                registroStage.setScene(new Scene(root, 458, 100));
-                registroStage.show();
+                if(!city.isEmpty()){
+                    con.GUARDAR("UPDATE sede SET ciudad = '"+city+"'" +
+                            "WHERE id_sede = '"+name+"'");
+                }
+                if(!telephoneNumber.isEmpty()){
+                    con.GUARDAR("UPDATE sede SET telefono = '"+telephoneNumber+"'" +
+                            "WHERE id_sede = '"+name+"'");
+                }
+                if(!address.isEmpty()){
+                    con.GUARDAR("UPDATE sede SET direccion = '"+address+"'" +
+                            "WHERE id_sede = '"+name+"'");
+                }
+                id_sede.setText("");
+                ciudad.setText("");
+                telefono.setText("");
+                direccion.setText("");
+                Parent root = FXMLLoader.load(getClass().getResource("/vista/updateUserWindow.fxml"));
+                Stage actualizacionStage = new Stage();
+                actualizacionStage.setTitle("Actualización");
+                actualizacionStage.setScene(new Scene(root, 458, 100));
+                actualizacionStage.show();
 
+                con.CERRAR();
             }
         } catch (IOException throwables) {
             throwables.printStackTrace();
