ec402824b40488ee5355a4e19a61f22ba91a5500
Nicolas <nicolas.jaramillo@correounivalle.edu.co>
2021-05-09 19:01:28 -0500

    Consultar Sedes working

--- src/modelo/Sedes.java
+++ src/modelo/Sedes.java
@@ -1,4 +1,5 @@
 package modelo;
+import controlador.OfficesManagement;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
@@ -10,6 +11,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Sedes {
+
+    private StringProperty id_sede;
     private StringProperty ciudad;
     private StringProperty telefono;
     private StringProperty direccion;
@@ -20,66 +23,66 @@ public class Sedes {
 	    VALUES ('890399001', 'Buga', '1234567', 'Avenida de porky #10-40')
 	*/
 
-    public Sedes(String ciudad, String telefono, String direccion)
-    {
+    public Sedes(String id_sede,String ciudad, String telefono, String direccion) {
+        this.id_sede = new SimpleStringProperty(id_sede);
         this.ciudad = new SimpleStringProperty(ciudad);
         this.telefono = new SimpleStringProperty(telefono);
         this.direccion = new SimpleStringProperty(direccion);
     }
 
     //getters
+    public String getId_sede() {
+        return id_sede.get();
+    }
+    
     public String getCiudad() {
         return ciudad.get();
     }
+
     public String getTelefono() {
         return telefono.get();
     }
+
     public String getDireccion() {
         return direccion.get();
     }
 
     //setters
+    public void setId_sede(String id_sede) {
+        this.id_sede.set(id_sede);
+    }
+
     public void setCiudad(String ciudad) {
         this.ciudad.set(ciudad);
     }
+
     public void setTelefono(String telefono) {
         this.telefono.set(telefono);
     }
+
     public void setDireccion(String direccion) {
         this.direccion.set(direccion);
     }
 
+    public static void setSedes(Sedes sedes) {
+        Sedes.sedes = sedes;
+    }
+
     //property
+    public StringProperty id_sedeProperty() {
+        return id_sede;
+    }
+
     public StringProperty ciudadProperty() {
         return ciudad;
     }
+
     public StringProperty telefonoProperty() {
         return telefono;
     }
+
     public StringProperty direccionProperty() {
         return direccion;
     }
+
 }
-
-    public static ObservableList<Sedes> fillTableSedes() {
-        Connect con  = new Connect();
-        //Se inicializa una colección de datos de tipo Sedes aún vacía (clases de colección)
-        ObservableList<Sedes> listaSedes = FXCollections.observableArrayList();
-        try {
-            String sql = "SELECT id_sede, ciudad, telefono,direccion FROM sede" ;
-
-            ResultSet resultado = con.CONSULTAR(sql);
-            while (resultado.next()){
-                listaSedes.add(
-                        new Sedes(resultado.getString("ciudad"),
-                                resultado.getString("telefono"),
-                                resultado.getString("direccion")
-                        )
-                );
-            }
-        }
-        catch (SQLException ex){
-            Logger.getLogger(Sedes.class.getName()).log(Level.SEVERE,null, ex);
-        }
-        return listaSedes;
-    }
